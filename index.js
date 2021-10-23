e6ef3e07ff72cfceed25c3138dbefc8448d3f84c
adrian <adrian.vhvhvh@gmail.com>
2021-10-23 18:32:35 -0300

    v 1.0.2

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require('express')
 const connectDB = require('./config/db')
-// const app = express();
+
 
 connectDB();
 
-// const port = process.env.PORT || 4000
-
-// app.listen (port, ()=>{
-//     console.log(`Server start at port ${port}`)
-// })
 
 
